3b2d4545b2f17f158fa486ca0a12740f266c153f
Dragos Tudorache <hedphug@gmail.com>
2015-12-15 17:44:31 +0200

    add ExDropbox.Api.Base module

---
+++ lib/ex_dropbox/api/base.ex
@@ -0,0 +1,12 @@
+defmodule ExDropbox.Api.Base do
+  @moduledoc """
+    Base module that acts as a mediator for the api resources
+  """
+  def get do
+    IO.puts "should build an url and make the actual request to dropbox"
+  end
+
+  def post do
+    IO.puts "should build an url and make the actual request to dropbox"
+  end
+end
