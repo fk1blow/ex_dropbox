2033b09e06e53294d421facf6dc9bccc53be2b0a
Dragos Tudorache <hedphug@gmail.com>
2015-12-16 23:30:11 +0200

    remove junk and tbd code

--- lib/ex_dropbox/api/account.ex
+++ lib/ex_dropbox/api/account.ex
@@ -2,14 +2,8 @@ defmodule ExDropbox.Api.Account do
   import ExDropbox.Api.Base
   import ExDropbox.Api.Endpoints
 
-  # use ExDropbox.Api.Resourceable
-
-  def get_account_info do
+  def account_info do
     # get "http://localhost:9000", "/account/info"
     get api_hostname, "/account/info"
-  end
-
-  def handle_account_info(info) do
-    #
   end
 end
