33da0cf3d89b643b449c0e6c36622a7a2bd0d900
Dragos Tudorache <hedphug@gmail.com>
2015-12-16 23:31:35 +0200

    refactor validate_request/1 to test for bitstring instead of length; refactor make_request/3 responses

--- lib/ex_dropbox/api/base.ex
+++ lib/ex_dropbox/api/base.ex
@@ -1,21 +1,18 @@
 defmodule ExDropbox.Api.Base do
-  import ExDropbox.Api.Endpoints
-  alias ExDropbox.Configuration, as: Config
-
   @moduledoc """
     Base module that acts as a mediator for the api resources
   """
+  import ExDropbox.Api.Endpoints
 
   def get(api_host, api_resource) do
-    Config.get[:access_token]
+    ExDropbox.Configuration.get[:access_token]
     |> validate_request
     |> make_request(api_host, api_resource)
   end
 
   defp validate_request(access_token) do
-    token_length = String.length(access_token)
     case access_token do
-      token when token_length > 1 -> token
+      token when is_bitstring(token) -> token
       nil -> {:error, "invalid or undefined access token"}
       _ -> {:error, "invalid access token"}
     end
@@ -24,14 +21,16 @@ defmodule ExDropbox.Api.Base do
   defp make_request({:error, reason}, _, _), do: {:error, reason}
 
   defp make_request(token, api_host, api_resource) do
-    {:ok, "should perform request"}
-    # case HTTPoison.get(url, headers) do
-    #   {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
-    #     IO.puts body
-    #   {:ok, %HTTPoison.Response{status_code: 404}} ->
-    #     IO.puts "Not found :("
-    #   {:error, %HTTPoison.Error{reason: reason}} ->
-    #     IO.inspect "reason is: #{reason}"
-    # end
+    headers = %{"Authorization" => "Bearer #{token}"}
+    url = api_host <> api_resource
+
+    case HTTPoison.get(url, headers) do
+      {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
+        {:ok, body}
+      {:ok, %HTTPoison.Response{status_code: 404}} ->
+        {:error, "resource not found"}
+      {:error, %HTTPoison.Error{reason: reason}} ->
+        {:error, "connection refused"}
+    end
   end
 end
