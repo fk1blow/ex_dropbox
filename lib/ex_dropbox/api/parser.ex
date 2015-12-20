152765348d2417c649857c48959e95d49c46fad4
Dragos Tudorache <hedphug@gmail.com>
2015-12-20 04:03:33 +0200

    refactor to_map's params names

--- lib/ex_dropbox/api/parser.ex
+++ lib/ex_dropbox/api/parser.ex
@@ -2,28 +2,28 @@ defmodule ExDropbox.Api.Parser do
   @doc """
     to_map when the resource wants to specify extraction of specific fields
   """
-  def to_map({:ok, resource_string}, resource_fields) do
-    case Poison.decode resource_string do
-      {:ok, resource} -> resource |> Dict.take resource_fields
+  def to_map({:ok, string}, fields) do
+    case Poison.decode string do
+      {:ok, resource} -> resource |> Dict.take fields
       {:error, reason} -> {:error, reason}
     end
   end
 
-  def to_map({:error, resource_string}, resource_fields) do
-    {:error, resource_string}
+  def to_map({:error, string}, fields) do
+    {:error, string}
   end
 
   @doc """
     to_map for cases when a resource doesn't need specific fields
   """
-  def to_map({:ok, resource_string}) do
-    case Poison.decode resource_string do
+  def to_map({:ok, string}) do
+    case Poison.decode string do
       {:ok, resource} -> resource
       {:error, reason} -> {:error, reason}
     end
   end
 
-  def to_map({:error, resource_string}) do
-    {:error, resource_string}
+  def to_map({:error, string}) do
+    {:error, string}
   end
 end
