243b353a92c4b87c3a160d541c52b7e3906c3e5d
Dragos Tudorache <hedphug@gmail.com>
2015-12-26 19:59:38 +0200

    remove even more unnecessary function from the base.ex module

--- lib/ex_dropbox/api/base.ex
+++ lib/ex_dropbox/api/base.ex
@@ -3,14 +3,6 @@ defmodule ExDropbox.Api.Base do
     HTTPoison.get(url, headers) |> handle_response
   end
 
-  # do not add the auth access token in here
-  defp handle_request({:error, reason}, _, _), do: {:error, reason}
-
-  defp handle_request(token, endpoint, params) do
-    headers = %{"Authorization" => "Bearer #{token}"}
-    HTTPoison.get(endpoint <> request_params(params), headers)
-  end
-
   @doc """
     handle response from httpoison call
     TODO: make shure you cover every response type dropbox may return
